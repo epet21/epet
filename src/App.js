f8931585e68a0ce836813fc4ab17f41363de450a
Shaun S <shaunseeram@hotmail.com>
2021-07-28 21:40:21 -0400

    Fixed health bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Main from "./components/Main";
-import Hatched from "./components/Hatched";
+// import Hatched from "./components/Hatched";
+import Hatched2 from "./components/Hatched2";
 import Header from './components/Header';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <Header />
       <Route exact path="/" component={Main} />
-      <Route exact path="/main" component={Hatched} />
+      {/* <Route exact path="/main" component={Hatched} /> */}
+      <Route exact path="/main" component={Hatched2} />
     </Router>
   );
 }
