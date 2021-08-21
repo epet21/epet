f0366f689f8c87d7c7d65edd8a0f22dfa0f6e316
MichelleChiasson <mitch.inbox@gmail.com>
2021-08-20 21:22:28 -0400

    Added some styles. images, and sprites

--- src/components/PhotoDisplayer.js
+++ src/components/PhotoDisplayer.js
@@ -1,21 +1,21 @@
-import bb from '../assets/bb.png';
-import child from '../assets/child.png';
-import teen from '../assets/teen.jpg';
-import adult from '../assets/adult.jpg';
+import bb from '../assets/charmander.gif';
+import child from '../assets/charmeleon.gif';
+import teen from '../assets/charizard.gif';
+import adult from '../assets/charizardMega.gif';
 import sickPet from '../assets/sickPet.png';
 
 const PhotoDisplayer = ({ stage, sick }) => {
   const imageDisplayer = () => {
     if (sick) {
-      return <img src={sickPet} alt="sick pet" />;
+      return <img className="petImage" src={sickPet} alt="sick pet" />;
     } else if (stage === 1) {
-      return <img src={bb} alt="smol bb pet" />;
+      return <img className="petImage" src={bb} alt="smol bb pet" />;
     } else if (stage === 2) {
-      return <img src={child} alt="child pet" />;
+      return <img className="petImage" src={child} alt="child pet" />;
     } else if (stage === 3) {
-      return <img src={teen} alt="teen pet" />;
+      return <img className="petImage" src={teen} alt="teen pet" />;
     } else if (stage === 4) {
-      return <img src={adult} alt="adult pet" />;
+      return <img className="petImage" src={adult} alt="adult pet" />;
     }
   };
 
